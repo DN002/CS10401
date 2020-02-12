6d40f15397d96487bf891737b8f1aa2e2a13391d
DN002 <60702489+DN002@users.noreply.github.com>
2020-02-12 09:37:51 -0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,7 @@
+#This is a comment
+temp = input("Please enter the current temperature: ")
+temp = int(temp)
+if temp >= 70:
+    print ("No jacket required")
+if temp < 70:
+    print ("Wear a jacket")
